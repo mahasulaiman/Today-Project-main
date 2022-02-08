d0ed8d9d6f8ce1d4555ab321f3264da2be77494a
mahasulaiman <mhao_h@hotmail.com>
2022-02-08 03:28:33 +0300

    add all changes

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const newsKey = "cfef1bff4c5b4900818b96ec6f1d80e2"
 const newsApi = `https://newsapi.org/v2/everything?q=tesla&from=2022-01-07&sortBy=publishedAt&apiKey=${newsKey}`
 let categoryURL = `https://newsapi.org/v2/top-headlines?category=general&apiKey=cfef1bff4c5b4900818b96ec6f1d80e2`
-        let headings = document.querySelectorAll("#heading a");
-        for (let heading of headings) {
-            heading.addEventListener("click", (event) => {
-                let category = event.target.id;
-                console.log(category);
-                categoryURL = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=cfef1bff4c5b4900818b96ec6f1d80e2`
-                fetchCategory()
-            })
-        }
-        function fetchCategory() {
-            fetch(categoryURL).then(res=>{
-            res.json().then(data=>{
-            console.log(data,"cat");
+let headings = document.querySelectorAll("#heading a");
+for (let heading of headings) {
+    heading.addEventListener("click", (event) => {
+        let category = event.target.id;
+        console.log(category);
+        categoryURL = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=cfef1bff4c5b4900818b96ec6f1d80e2`
+        fetchCategory()
+    })
+}
+function fetchCategory() {
+    fetch(categoryURL).then(res => {
+        res.json().then(data => {
+            console.log(data, "cat");
             document.getElementById("mewsCard").innerHTML = data.articles.map(article => `
             <div class="col-md-6 my-2">
             <div class="card" >
@@ -29,7 +29,42 @@ let categoryURL = `https://newsapi.org/v2/top-headlines?category=general&apiKey=
             </div>
             </div>
             `
-                            ).join("")
-                        })
-                    })
-                }
+            ).join("")
+        })
+    })
+}
+
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273;
+
+window.addEventListener("load", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            const api = "6d055e39ee237af35ca066f35474e9df";
+            const base = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+                `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+            fetch(base)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    temperature.textContent =
+                        Math.floor(data.main.temp - kelvin) + "°C";
+                    summary.textContent = data.weather[0].description;
+                    loc.textContent = data.name + "," + data.sys.country;
+                    let icon1 = data.weather[0].icon;
+                    icon.innerHTML =
+                        `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
+                });
+        });
+    }
+});
